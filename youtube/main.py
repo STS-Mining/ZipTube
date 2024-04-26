73e0f61f5579d4eaf29cc64cff489cb3b5462a20
Steve Sawaya <sawayatransport@gmail.com>
2024-04-27 04:59:58 +1000

    error handling added to buttons

--- youtube/main.py
+++ youtube/main.py
@@ -1,5 +1,6 @@
 # Author: STS-Mining
-# Version 1.11
+# Date: 2024-04-27
+# ZipTube Version 1.18
 
 import customtkinter as ctk
 from pytube import YouTube
@@ -9,13 +10,19 @@ import pyperclip
 from PIL import Image
 import os
 
+# Icon and logo location on system
 icon = "C:/Python/Stuff/youtube/img/icon.ico"
 logo = "C:/Python/Stuff/youtube/img/logo.png"
 
+# Function that downloads the video once the download button is pressed
 def download_video(resolutions_var):
     global download_button, donation_button
     url = entry_url.get()
     resolution = resolutions_var.get()
+    if not resolution:  # Check if resolution is not selected
+        messagebox.showerror("Error", "Please select a resolution.")
+        return
+
     progress_label.pack(pady="10p")
     status_label.pack(pady="10p")
     donation_button.pack(pady="10p")
@@ -33,19 +40,22 @@ def download_video(resolutions_var):
         status_label.configure(text=f"File saved as: {yt.title}-{resolution}")
     except Exception:
         status_label.configure(text=f"Error, resolution selected doesn't exist for video ...", text_color="red")
-        # Schedule hiding labels after 10 seconds
-        app.after(5000, hide_labels)
+        # Schedule hiding labels after 2 seconds
+        app.after(2000, hide_labels)
 
+# Function while the download is in progress
 def on_progress(stream, chunk, bytes_remaining):
     global start_time, bytes_downloaded_prev, download_button
+    download_button.configure(state='disabled')  # Disable the download button
+    resolutions_button.configure(state='disabled')  # Disable the resolutions button
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     progress_percentage = (bytes_downloaded / total_size) * 100
     download_finished = (bytes_downloaded == total_size)
     if download_finished:
         download_button.configure(text="Download Complete!", border_color="#00d11c")
-        # Schedule hiding labels after 10 seconds
-        app.after(10000, hide_labels)
+        # Schedule hiding labels after 3 seconds
+        app.after(3000, hide_labels)
     else:
         download_button.configure(text=f"Downloading ... {int(progress_percentage)}%", border_color="yellow")
         current_time = time.time()
@@ -61,6 +71,7 @@ def on_progress(stream, chunk, bytes_remaining):
                 ))
         progress_label.update()
         status_label.configure(text=f"Saving to local Downloads folder ...")
+
 
 # Function to ask for confirmation before closing the window
 def on_close():
@@ -107,12 +118,21 @@ def open_donation_window():
     # Start the donation window's main loop
     donation_window.mainloop()
 
+# Hide the labels after 3 seconds
 def hide_labels():
+    global resolutions_var
     status_label.pack_forget()  # Hide the status label
     progress_label.pack_forget()  # Hide the progress label
-    download_button.pack_forget()  # Hide the download button
+    resolutions_button.pack_forget()  # Hide the resolutions button
+    download_button.configure(state='normal')  # Enable the download button
+    download_button.configure(text="Download Another Video?", border_color="#00d11c", command=start_app_again)
     donation_button.pack_forget()  # Hide the donation button
+    resolutions_frame.pack_forget()  # Hide the resolutions frame
+    entry_url.delete(0, ctk.END)  # Clear the entry URL
+    resolutions_var.set("")  # Clear the resolutions variable
+    donation_button.pack(pady="10p")  # Show the donation button
 
+# Calculate the nearest measurement for bytes
 def bytes_to_nearest_measurement(bytes):
     megabytes = bytes / (1024 * 1024)
     gigabytes = bytes / (1024 * 1024 * 1024)
@@ -121,6 +141,7 @@ def bytes_to_nearest_measurement(bytes):
     else:
         return "{} MB".format(round(megabytes))
 
+# Function to print all available resolutions for a YouTube video
 def print_available_resolutions(url):
     try:
         yt = YouTube(url)
@@ -133,7 +154,6 @@ def print_available_resolutions(url):
             resolutions_var.set(resolution)
 
         selected_resolution = ctk.StringVar()
-        resolutions_frame = ctk.CTkFrame(content_frame)
         resolutions_frame.pack(pady=10)
 
         for i, resolution in enumerate(resolutions):
@@ -147,9 +167,23 @@ def print_available_resolutions(url):
 # Function to print all available resolutions for a YouTube video
 def load_resolutions():
     global resolutions_var
-    # Call print_available_resolutions to print available resolutions
-    resolutions_var = print_available_resolutions(entry_url.get())
-    download_button.pack(pady=10)
+    url = entry_url.get().strip()  # Get the URL and remove leading/trailing whitespace
+    if not url:
+        messagebox.showerror("Error", "Please enter a YouTube video URL.")
+    else:
+        # Call print_available_resolutions to print available resolutions
+        resolutions_var = print_available_resolutions(url)
+        download_button.pack(pady=10)
+
+# Function to start a new download
+def start_app_again():
+    print("Starting app again...")
+    donation_button.pack_forget()  # Hide the donation button
+    download_button.pack_forget()  # Hide the download button
+    resolutions_button.configure(state='normal')  # Enable the resolutions button
+    resolutions_button.configure(text="Load Resolutions", command=load_resolutions, border_color="#FFCC70")
+    resolutions_button.pack(pady=10)  # Show the resolutions button
+    download_button.configure(text="Download", command=lambda: download_video(resolutions_var))
 
 # Create a app window
 app = ctk.CTk()
@@ -179,14 +213,18 @@ logo_image = ctk.CTkImage(pil_image, size=(300, 75))
 heading = ctk.CTkLabel(content_frame, image=logo_image, text="")
 heading.pack(pady="10p")
 
+# Create a label and the entry widget for the video url
 entry_url = ctk.STsEntry(content_frame, placeholder_text=("Paste URL here..."))
 entry_url.pack(pady="10p")
 
+# Create a resolutions frame to hold the resolutions
+resolutions_frame = ctk.CTkFrame(content_frame)
+
 # Create a download button
-download_button = ctk.CTkButton(content_frame, text="Download", command=lambda: download_video(resolutions_var))
+download_button = ctk.CTkButton(content_frame, border_color="#00d11c", text="Download", command=lambda: download_video(resolutions_var))
 
 # Create a resolutions button
-resolutions_button = ctk.CTkButton(content_frame, text="Load Resolutions", command=load_resolutions)
+resolutions_button = ctk.CTkButton(content_frame, text="Load Resolutions", command=load_resolutions, border_color="#FFCC70")
 resolutions_button.pack(pady="10p")
 
 # Define resolutions_var globally
@@ -200,6 +238,9 @@ status_label = ctk.CTkLabel(content_frame, text="")
 
 # Create a donate button
 donation_button = ctk.CTkButton(content_frame, text="Donate", command=open_donation_window, border_color="red")
+
+# Create a hello button
+hello_button = ctk.CTkButton(content_frame, text="Download Another Video?", command=start_app_again, border_color="#00d11c")
 
 # Add the on_close function to the close button
 app.protocol("WM_DELETE_WINDOW", on_close)
