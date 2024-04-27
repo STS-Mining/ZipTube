e2995847c0a86813a79485b1e9892e6b046456d9
Steve Sawaya <sawayatransport@gmail.com>
2024-04-28 07:02:22 +1000

    fix path to images

--- youtube/main.py
+++ youtube/main.py
@@ -12,8 +12,8 @@ import os
 import re
 
 # Icon and logo location on system
-icon = "assets/images/icon.ico"
-logo = "assets/images/logo.png"
+icon = "youtube/assets/images/icon.ico"
+logo = "youtube/assets/images/logo.png"
 
 # Function that downloads the video once the download button is pressed
 def download_video(resolutions_var):
